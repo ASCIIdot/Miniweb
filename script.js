52f859496334d63dfbdebb15bc2183c9932c967a
ASCIIdot <92599559+ASCIIdot@users.noreply.github.com>
2022-06-21 23:05:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,95 +1,278 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() { stick() };
-
-// Get the header
-var nav = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = nav.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function stick() {
-    if (window.pageYOffset > sticky) {
-        nav.classList.add("sticky");
-    } else {
-        nav.classList.remove("sticky");
-    }
-}
-
-
-
-class TypeWriter {
-    constructor(txtElement, words, wait = 3000) {
-        this.txtElement = txtElement;
-        this.words = words;
-        this.txt = '';
-        this.wordIndex = 0;
-        this.wait = parseInt(wait, 10);
-        this.type();
-        this.isDeleting = false;
-    }
-
-    type() {
-        // Current index of word
-        const current = this.wordIndex % this.words.length;
-        // Get full text of current word
-        const fullTxt = this.words[current];
-
-        // Check if deleting
-        if (this.isDeleting) {
-            // Remove char
-            this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-            // Add char
-            this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
-
-        // Insert txt into element
-        this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
-
-        // Initial Type Speed
-        let typeSpeed = 300;
-
-        if (this.isDeleting) {
-            typeSpeed /= 2;
-        }
-
-        // If word is complete
-        if (!this.isDeleting && this.txt === fullTxt) {
-            // Make pause at end
-            typeSpeed = this.wait;
-            // Set delete to true
-            this.isDeleting = true;
-        } else if (this.isDeleting && this.txt === '') {
-            this.isDeleting = false;
-            // Move to next word
-            this.wordIndex++;
-            // Pause before start typing
-            typeSpeed = 500;
-        }
-
-        setTimeout(() => this.type(), typeSpeed);
-    }
-}
-
-
-// Init On DOM Load
-document.addEventListener('DOMContentLoaded', init);
-
-// Init App
-function init() {
-    const txtElement = document.querySelector('.txt-type');
-    const words = JSON.parse(txtElement.getAttribute('data-words'));
-    const wait = txtElement.getAttribute('data-wait');
-    // Init TypeWriter
-    new TypeWriter(txtElement, words, wait);
-}
-
-
-// const canvas = document.getElementById('pfp');
-// const context = canvas.getContext('2d');
-// const img = new Image();
-// img.src = 'pfp.png';
-// img.onload = () => {
-//     context.drawImage(img, 0, 0, 320, 320);
-// };
+ @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@300&display=swap');
+ @import url('https://fonts.googleapis.com/css2?family=Special+Elite&display=swap');
+ @import url('https://fonts.googleapis.com/css2?family=Barriecito&display=swap');
+ * {
+     margin: 0;
+     padding: 0;
+ }
+ /* #FCF7F8 – snow
+#FFC5CD – l.pink
+#A2989A – mid grey
+#2F2D2D – charc
+#000000 -- black
+ */
+ 
+ body {
+     background-color: #0e0d0d;
+     color: #FCF7F8;
+     display: grid;
+     grid-template-areas: "header" "nav" "main" "footer";
+     grid-template-rows: auto auto auto 200px;
+     overflow-x: hidden;
+     position: relative;
+ }
+ 
+ .header {
+     background-image: linear-gradient(to bottom, rgba(245, 246, 252, 0.00), rgba(0, 0, 0, 1)), url(https://media.discordapp.net/attachments/754655109312675872/987790994323931217/IMG_2200.png);
+     height: 60vh;
+     background-color: #A2989A;
+     font-size: 30px;
+     display: flex;
+ }
+ 
+ #heading {
+     align-self: flex-end;
+     font-family: 'Special Elite', cursive;
+     grid-area: header;
+     width: 100vw;
+ }
+ 
+ .txt-type {
+     font-family: 'Special Elite', cursive;
+ }
+ 
+ .navbar {
+     background-color: #000000;
+     color: #FCF7F8;
+     display: flex;
+     justify-content: space-evenly;
+     width: 100vw;
+     height: 6vh;
+     font-family: 'Barriecito', cursive;
+     font-size: 30px;
+     grid-area: nav;
+     z-index: 100;
+     position: sticky;
+ }
+ /* The sticky class is added to the navbar with JS when it reaches its scroll position */
+ 
+ .sticky {
+     position: fixed;
+     top: 0;
+ }
+ /* Add some top padding to the page content to prevent sudden quick movement (as the navigation bar gets a new position at the top of the page (position:fixed and top:0) */
+ 
+ .txt-type>.txt {
+     border-right: 0.2rem solid #FFC5CD;
+     animation: blinker 1s linear infinite;
+ }
+ 
+ @keyframes blinker {
+     from,
+     to {
+         border-color: transparent
+     }
+     50% {
+         border-color: #FFC5CD;
+     }
+ }
+ 
+ #pfp {
+     justify-self: start;
+     background-color: #FFC5CD;
+     grid-area: pfp;
+     height: 20em;
+     width: 20em;
+     align-self: center;
+     border: 6px solid #000000;
+     box-shadow: rgba(255, 197, 205, 0.4) 5px 5px, rgba(255, 197, 205, 0.3) 10px 10px, rgba(255, 197, 205, 0.2) 15px 15px, rgba(255, 197, 205, 0.1) 20px 20px, rgba(255, 197, 205, 0.05) 25px 25px;
+     border-radius: 15px;
+     z-index: 10;
+ }
+ 
+ #content {
+     display: grid;
+     justify-content: stretch;
+     grid-template-areas: "pfp desc desc" "rundown rundown rundown" "more null spotify";
+     grid-template-columns: 1fr 1fr 1fr;
+     margin-top: 5em;
+     row-gap: 4em;
+     column-gap: 2em;
+     width: 80%;
+     margin-left: 7em;
+     grid-area: main;
+     /* border: 6px solid #000000;
+     border-width: 6px;
+     border-style: solid;
+     border-image: linear-gradient( to bottom, #1f1d1d, #FFC5CD) 1 100%;
+     z-index: 9;
+     background-image: linear-gradient(to bottom, #0e0d0d, #1f1d1d, #FFC5CD);
+     border-bottom-style: none;
+     padding: 10px;
+     border-top-style: none; */
+ }
+ 
+ #desc {
+     grid-area: desc;
+     justify-self: start;
+     align-self: center;
+     font-family: 'Barriecito', cursive;
+     font-size: 1.4em;
+     text-shadow: 0.15em 0.15em #000000;
+     border-radius: 15px;
+     z-index: 10;
+     line-height: 3em;
+ }
+ 
+ .footer {
+     width: 100vw;
+     position: absolute;
+     bottom: 0;
+ }
+ 
+ #cloud {
+     width: 110vw;
+     z-index: 1;
+     bottom: 190px;
+     position: inherit;
+ }
+ 
+ #footercontent {
+     grid-area: footer;
+     z-index: 2;
+     background-color: #FFC5CD;
+     width: 100vw;
+     height: 15em;
+ }
+ 
+ .spotify {
+     border: 6px solid #000000;
+     grid-area: spotify;
+ }
+ 
+ #rundown {
+     grid-area: rundown;
+     padding: 1em;
+     background-color: #fff781;
+     color: #2F2D2D;
+     border: 6px solid #000000;
+     border-radius: 15px;
+     font-family: 'Barriecito', cursive;
+     font-size: 20px;
+     z-index: 10;
+ }
+ 
+ #hxs {
+     grid-area: more;
+     padding-left: 2em;
+     padding-right: 2em;
+     padding-top: 1em;
+     padding-bottom: 1em;
+     background-color: #0e0d0d;
+     color: #FFC5CD;
+     border: 6px solid #FFC5CD;
+     border-radius: 15px;
+     font-family: 'Barriecito', cursive;
+     font-size: 20px;
+     z-index: 10;
+     line-height: 1.5em;
+     position: relative;
+ }
+ 
+ #kinnies {
+     grid-area: null;
+     padding-left: 2em;
+     padding-right: 2em;
+     padding-top: 1em;
+     padding-bottom: 1em;
+     background-color: #FFC5CD;
+     color: #0e0d0d;
+     border: 6px solid #000000;
+     border-radius: 15px;
+     font-family: 'Barriecito', cursive;
+     font-size: 20px;
+     z-index: 10;
+     line-height: 1.5em;
+     position: relative;
+ }
+ 
+ .hr1 {
+     border: 0;
+     height: 16px;
+     width: auto;
+     background-image: url("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/cf2836cb-5893-4a6c-b156-5a89d94fc721/db1ecvu-dfa8e61c-f56c-4405-b300-adec0c758fda.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2NmMjgzNmNiLTU4OTMtNGE2Yy1iMTU2LTVhODlkOTRmYzcyMVwvZGIxZWN2dS1kZmE4ZTYxYy1mNTZjLTQ0MDUtYjMwMC1hZGVjMGM3NThmZGEuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.GfaghPyPh2GC6F3PXYIfa0omqKm8YgzmUiOzV6ZhQhY");
+     background-repeat: repeat-x;
+ }
+ 
+ #uwu {
+     position: absolute;
+     right: 1em;
+     bottom: 1em;
+ }
+ 
+ #owo {
+     border: 0;
+     height: 99px;
+     background-image: url(" https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/cf2836cb-5893-4a6c-b156-5a89d94fc721/db5i1a2-8911af03-5b6a-434b-ab28-591ff9df444a.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2NmMjgzNmNiLTU4OTMtNGE2Yy1iMTU2LTVhODlkOTRmYzcyMVwvZGI1aTFhMi04OTExYWYwMy01YjZhLTQzNGItYWIyOC01OTFmZjlkZjQ0NGEuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.Nrij1UdvZDJtWJvrUCGPI3nyPqOmdjZbivCDYHlMiqM");
+     background-repeat: repeat-x;
+ }
+ 
+ ul {
+     list-style-image: url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/768df49b-9b9a-45f9-b8b9-56558c7b0cfb/d79d9ns-78785fc4-ec74-4038-b0cf-cb36b53cb802.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzc2OGRmNDliLTliOWEtNDVmOS1iOGI5LTU2NTU4YzdiMGNmYlwvZDc5ZDlucy03ODc4NWZjNC1lYzc0LTQwMzgtYjBjZi1jYjM2YjUzY2I4MDIuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.RvnRSSEzk7Mxz-qT24OzuFIPmZ3rBBk6c56A7P0t3a4');
+ }
+ 
+ a:link {
+     color: #A2989A;
+     background-color: transparent;
+     text-decoration: none;
+ }
+ 
+ a:visited {
+     color: #FFC5CD;
+     background-color: transparent;
+     text-decoration: none;
+ }
+ 
+ a:hover {
+     color: rgb(145, 242, 247);
+     background-color: transparent;
+ }
+ 
+ a:active {
+     color: #fff781;
+     background-color: transparent;
+ }
+ /* kjfner*/
+ /* code break */
+ 
+ nav a {
+     text-decoration: none;
+     color: rgb(255, 255, 255);
+     position: relative;
+     display: inline-block;
+     padding: 10px 40px;
+ }
+ 
+ nav a::after {
+     content: "";
+     background: #FFC5CD;
+     height: 1px;
+     position: absolute;
+     bottom: 0;
+     transition: .16s all 0.025s;
+ }
+ 
+ nav a::after {
+     left: 100%;
+     right: 0;
+ }
+ 
+ nav a:hover~a::after {
+     left: 0;
+     right: 100%;
+ }
+ 
+ nav a:hover::after {
+     left: 0;
+     right: 0;
+ }
